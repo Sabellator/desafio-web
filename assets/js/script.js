c64c30c61646170e08a9ddcd5ede1d8d55bac324
Ulises <sabelle07@gmail.com>
2023-08-18 14:34:31 -0400

    mejoras en cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,29 @@ function actualizarFechaActual() {
 
 actualizarFechaActual();
 setInterval(actualizarFechaActual, 1000);
+
+// FORMULARIO
+const form = document.getElementById("contact-form");
+const correoInput = document.getElementById("correo");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (!form.checkValidity()) {
+    alert("Por favor, llene todos los campos correctamente.");
+  } else if (!validarCorreo(correoInput.value)) {
+    alert(
+      "Por favor, ingrese un correo válido: con @ y extensión final después del punto."
+    );
+  } else {
+    alert("El correo fue enviado exitosamente (éste es un formulario de prueba con fines académicos. Para contactarme, por favor, usar mis datos personales, ubicados en el footer).");
+    limpiarFormulario(); 
+  }
+});
+function validarCorreo(correo) {
+  const regexCorreo = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{1,3}$/;
+  return regexCorreo.test(correo);
+}
+function limpiarFormulario() {
+  form.reset(); 
+}
+
